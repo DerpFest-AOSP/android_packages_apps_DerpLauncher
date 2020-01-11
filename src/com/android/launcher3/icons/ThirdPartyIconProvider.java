673147f90e65d0467ff4a825609d3defed36d576
Amir Zaidi <azaidi@live.nl>
2020-01-11 13:55:58 +0100

    DerpLauncher: Add Icon Pack Support from Shade Launcher [SQUASHED]
    DennySPb: port to A11, A12-L, A13
    
    neobuddy89:
    Updated for A13 QPR2
    
    ikeramat: port to A15-QPR1
    
    [HELLBOY017]: Adapt for A16 (Switch to DaggerSingletonObject)
    
    Change-Id: Id81da397d5ef33577d6f53f349080c49a61591ab
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

---
+++ src/com/android/launcher3/icons/ThirdPartyIconProvider.java
@@ -0,0 +1,58 @@
+package com.android.launcher3.icons;
+
+import android.content.Context;
+import android.content.pm.LauncherActivityInfo;
+import android.graphics.drawable.Drawable;
+
+import android.annotation.SuppressLint;
+import com.android.launcher3.util.ComponentKey;
+
+import com.android.launcher3.dagger.ApplicationContext;
+import com.android.launcher3.dagger.LauncherAppSingleton;
+import com.android.launcher3.graphics.ThemeManager;
+import com.android.launcher3.icons.pack.IconResolver;
+import com.android.launcher3.util.ApiWrapper;
+
+import javax.inject.Inject;
+
+import static com.android.launcher3.icons.BaseIconFactory.CONFIG_HINT_NO_WRAP;
+
+@LauncherAppSingleton
+public class ThirdPartyIconProvider extends RoundIconProvider {
+
+    private final Context mContext;
+    private final ThemeManager mThemeManager;
+    private final ApiWrapper mApiWrapper;
+
+    @Inject
+    public ThirdPartyIconProvider(
+            @ApplicationContext Context context,
+            ThemeManager themeManager,
+            ApiWrapper apiWrapper) {
+        super(context, themeManager, apiWrapper);
+        mContext = context;
+        mThemeManager = themeManager;
+        mApiWrapper = apiWrapper;
+    }
+
+    public ThirdPartyIconProvider(Context context) {
+        this(context, ThemeManager.INSTANCE.get(context), ApiWrapper.INSTANCE.get(context));
+    }
+
+    @SuppressLint("WrongConstant")
+    @Override
+    public Drawable getIcon(LauncherActivityInfo launcherActivityInfo, int iconDpi) {
+        ComponentKey key = new ComponentKey(
+                launcherActivityInfo.getComponentName(), launcherActivityInfo.getUser());
+
+        IconResolver.DefaultDrawableProvider fallback =
+                () -> super.getIcon(launcherActivityInfo, iconDpi);
+        Drawable icon = ThirdPartyIconUtils.getByKey(mContext, key, iconDpi, fallback);
+
+        if (icon == null) {
+            return fallback.get();
+        }
+        icon.setChangingConfigurations(icon.getChangingConfigurations() | CONFIG_HINT_NO_WRAP);
+        return icon;
+    }
+}
