1d5052fe71bbe15bf85a008a01eff252f4987e48
NurKeinNeid <mralexman3000@gmail.com>
2024-11-07 01:39:17 +0800

    SettingsActivity: Make title follow popup menu
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -191,7 +191,7 @@ public class SettingsActivity extends FragmentActivity
             }
 
             if (getActivity() != null) {
-                getActivity().setTitle(null);
+                getActivity().setTitle(getString(R.string.settings_title));
             }
         }
 
