801e1d9a1b29423428265f7c37194c86951be9e5
John Andrew Camu <werdna.jac@gmail.com>
2025-01-08 19:04:28 +0800

    DerpLauncher: Add wallpaper pop-up carousel
    neobuddy89:
    * Adapt DAO and Full Room for launcher3 AOSP compilation
    * Smoothen the carousel animation; only apply wallpaper on click
    * Rework wallpaper ranking completely
    * Fix icon frame attachment
    * Fix flickering and performance issues
    * Use single scope for carousel view
    * Various guard checks for views
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I45af4daccc0a674f26e01e403a61227b0b4c1f03
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -138,6 +138,7 @@ import com.android.launcher3.apppairs.AppPairIcon;
 import com.android.launcher3.appprediction.PredictionRowView;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.dagger.LauncherComponentProvider;
+import com.android.launcher3.data.AppDatabase;
 import com.android.launcher3.desktop.DesktopRecentsTransitionController;
 import com.android.launcher3.display.DisplayController;
 import com.android.launcher3.display.LauncherDisplayInfo;
@@ -795,6 +796,8 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
         OverviewComponentObserver.INSTANCE.get(this)
                 .addOverviewChangeListener(mOverviewChangeListener);
         new TraceStateLoggerHelper(this).startTraceStateLogger();
+
+        AppDatabase.INSTANCE.get(this).checkpointSync();
     }
 
     @Override
