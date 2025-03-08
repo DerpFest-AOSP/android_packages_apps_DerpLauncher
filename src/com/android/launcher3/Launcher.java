55fb7f15ec388a8dc8fc75ef363fb5e626b8a328
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-03-08 18:01:23 +0530

    DerpLauncher: Make auto opening keyboard faster
    Change-Id: I5d0477057d27088ab2b82bff903c302ceaf52806

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -431,6 +431,7 @@ public class Launcher extends StatefulActivity<LauncherState>
     private final List<BackPressHandler> mBackPressedHandlers = new ArrayList<>();
 
     private boolean mIsNaturalScrollingEnabled;
+    private boolean mIsKeyboardShown;
 
     private boolean mShouldUpdateSuspensions;
 
@@ -1180,6 +1181,16 @@ public class Launcher extends StatefulActivity<LauncherState>
             Animations.Companion.cancelOngoingAnimation(getWorkspace());
             Animations.Companion.cancelOngoingAnimation(getHotseat());
         }
+
+        // Show or hide the keyboard as soon as we start entering or exiting app drawer
+        if ((mIsKeyboardShown || ALL_APPS.equals(mPrevLauncherState)) && !ALL_APPS.equals(state)) {
+            hideKeyboard();
+            mIsKeyboardShown = false;
+        } else if (!mPrevLauncherState.equals(ALL_APPS) && state.equals(ALL_APPS)
+                && DRAWER_OPEN_KEYBOARD.get(this)) {
+            mIsKeyboardShown = getAppsView().getSearchUiManager().focusSearchField();
+        }
+
         updateDisallowBack();
     }
 
@@ -1221,8 +1232,10 @@ public class Launcher extends StatefulActivity<LauncherState>
             getAppsView().reset(false /* animate */, true /* clearScrim */);
             getAllAppsExitEvent().ifPresent(getStatsLogManager().logger()::log);
             mAllAppsSessionLogId = null;
-        } else if (ALL_APPS.equals(state) && DRAWER_OPEN_KEYBOARD.get(this)) {
-            getAppsView().getSearchUiManager().focusSearchField();
+        } else if (ALL_APPS.equals(state) && !mIsKeyboardShown
+               && DRAWER_OPEN_KEYBOARD.get(this)) {
+            // double check in case it didn't work in onStateSetStart()
+            mIsKeyboardShown = getAppsView().getSearchUiManager().focusSearchField();
         }
         setTitle(state);
     }
--- src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
+++ src/com/android/launcher3/allapps/search/AppsSearchContainerLayout.java
@@ -167,8 +167,8 @@ public class AppsSearchContainerLayout extends ExtendedEditText
     }
 
     @Override
-    public void focusSearchField() {
-        mSearchBarController.focusSearchField();
+    public boolean focusSearchField() {
+        return mSearchBarController.focusSearchField();
     }
 
     @Override
