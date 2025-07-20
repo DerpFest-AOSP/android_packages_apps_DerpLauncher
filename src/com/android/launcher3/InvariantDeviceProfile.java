DerpFest-AOSP/packages_apps_DerpLauncher

DerpLauncher: Add toggle for desktop and drawer labels
Co-authored-by: razorloves <razorloves@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Change-Id: I98063b7adaf22029c4bfa50d2cac730f3612e121

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -27,6 +27,8 @@ import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
 import android.annotation.TargetApi;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
@@ -80,7 +82,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class InvariantDeviceProfile {
+public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener {
 
     public static final String TAG = "IDP";
     // We do not need any synchronization for this variable as its only written on UI thread.
@@ -97,6 +99,8 @@ public class InvariantDeviceProfile {
 
     private static final float ICON_SIZE_DEFINED_IN_APP_DP = 48;
 
+    public static final String KEY_SHOW_DESKTOP_LABELS = "pref_desktop_show_labels";
+    public static final String KEY_SHOW_DRAWER_LABELS = "pref_drawer_show_labels";
     public static final String KEY_WORKSPACE_LOCK = "pref_workspace_lock";
 
     // Constants that affects the interpolation curve between statically defined device profile
@@ -214,6 +218,8 @@ public class InvariantDeviceProfile {
 
     public Point defaultWallpaperSize;
 
+    private Context mContext;
+
     private final ArrayList<OnIDPChangeListener> mChangeListeners = new ArrayList<>();
 
     private static final Uri ENABLE_TASKBAR_URI = Settings.System.getUriFor(
@@ -246,6 +252,10 @@ public class InvariantDeviceProfile {
 
     @TargetApi(23)
     private InvariantDeviceProfile(Context context) {
+        mContext = context;
+
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context);
+        prefs.registerOnSharedPreferenceChangeListener(this);
         String gridName = getCurrentGridName(context);
         String newGridName = initGrid(context, gridName);
         if (!newGridName.equals(gridName)) {
@@ -360,6 +370,13 @@ public class InvariantDeviceProfile {
             return TYPE_TABLET;
         } else {
             return TYPE_PHONE;
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (KEY_SHOW_DESKTOP_LABELS.equals(key) || KEY_SHOW_DRAWER_LABELS.equals(key)) {
+            onConfigChanged(mContext);
         }
     }
 

DerpFest-AOSP/android_packages_apps_DerpLauncher

src/com/android/launcher3/InvariantDeviceProfile.java
@@ -35,6 +35,8 @@ import static com.android.launcher3.util.Executors.MAIN_EXECUTOR;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.content.res.XmlResourceParser;
@@ -94,7 +96,7 @@ import java.util.stream.Collectors;
 import javax.inject.Inject;
 
 @LauncherAppSingleton
-public class InvariantDeviceProfile {
+public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener {
 
     public static final String TAG = "IDP";
     // We do not need any synchronization for this variable as its only written on UI thread.
@@ -116,6 +118,8 @@ public class InvariantDeviceProfile {
 
     private static final float ICON_SIZE_DEFINED_IN_APP_DP = 48;
 
+    public static final String KEY_SHOW_DESKTOP_LABELS = "pref_desktop_show_labels";
+    public static final String KEY_SHOW_DRAWER_LABELS = "pref_drawer_show_labels";
     public static final String KEY_WORKSPACE_LOCK = "pref_workspace_lock";
 
     // Constants that affects the interpolation curve between statically defined device profile
@@ -260,6 +264,8 @@ public class InvariantDeviceProfile {
 
     public Point defaultWallpaperSize;
 
+    private Context mContext;
+
     private final List<OnIDPChangeListener> mChangeListeners = new CopyOnWriteArrayList<>();
 
     @Inject
@@ -270,12 +276,15 @@ public class InvariantDeviceProfile {
             WindowManagerProxy wmProxy,
             ThemeManager themeManager,
             DaggerSingletonTracker lifeCycle) {
+        mContext = context;
         mDisplayController = dc;
         mWMProxy = wmProxy;
         mPrefs = prefs;
         mThemeManager = themeManager;
 
-        String gridName = prefs.get(GRID_NAME);
+        SharedPreferences sharedPrefs = LauncherPrefs.getPrefs(context);
+        sharedPrefs.registerOnSharedPreferenceChangeListener(this);
+        String gridName = mPrefs.get(GRID_NAME);
         initGrid(context, gridName);
 
         dc.setPriorityListener(
@@ -362,6 +371,13 @@ public class InvariantDeviceProfile {
     @Deprecated
     public void reset(Context context) {
         initGrid(context, mPrefs.get(GRID_NAME));
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+        if (KEY_SHOW_DESKTOP_LABELS.equals(key) || KEY_SHOW_DRAWER_LABELS.equals(key)) {
+            onConfigChanged(mContext);
+        }
     }
 
     private void initGrid(Context context, Info displayInfo, DisplayOption displayOption) {