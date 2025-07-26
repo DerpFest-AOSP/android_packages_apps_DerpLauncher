BootleggersROM/packages_apps_Launcher3

Quickspace: Add contextual messages/greetings event
Bootleggers edit: Let's add some dumb message once in a while, and randomize the things quite a bit. Also, make it toggleable just because we need more clutter. Also, a nice icon for each special event
Co-authored-by: El Dainosor <eldainosor@gmail.com>

src/com/android/launcher3/quickspace/QuickEventsController.java
@@ -36,6 +36,7 @@ import com.android.launcher3.Launcher;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.R;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 import java.util.ArrayList;
@@ -58,6 +59,27 @@ public class QuickEventsController {
     // Device Intro
     private boolean mEventIntro = false;
     private boolean mIsFirstTimeDone; 
+    // PSA + Personality
+    private boolean mEventPSA = false;
+    private String mPSAMessage;
+    private Calendar mPSACalendar = Calendar.getInstance();
+    private int mPSARandom;
+    private Random mPSAGenerator;
+    private String[] mPSAMorningStr;
+    private String[] mPSAEvenStr;
+    private String[] mPSAMidniteStr;
+    private String[] mPSARandomStr;
+    private BroadcastReceiver mPSAListener = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Calendar mPSAOldCalendar = mPSACalendar;
+            mPSACalendar = Calendar.getInstance();
+            if (mPSACalendar != mPSAOldCalendar) {
+                psonalityEvent();
+            }
+        }
+
+    };
     // Now Playing
     private IntentFilter intent = new IntentFilter();
     private String[] intentWhitelist = new String[]{"com.android.music.metachanged", "com.android.music.playstatechanged", "com.android.music.playbackcomplete", "com.android.music.queuechanged", "com.jrtstudio.music.playstatechanged", "com.jrtstudio.music.playbackcomplete", "com.jrtstudio.music.metachanged", "com.htc.music.playstatechanged", "com.htc.music.playbackcomplete", "com.htc.music.metachanged", "fm.last.android.metachanged", "fm.last.android.playbackpaused", "fm.last.android.playbackcomplete", "com.lge.music.metachanged", "com.lge.music.playstatechanged", "com.lge.music.endofplayback", "com.miui.player.playbackcomplete", "com.miui.player.metachanged", "com.real.IMP.playstatechanged", "com.real.IMP.playbackcomplete", "com.real.IMP.metachanged", "com.sonyericsson.music.metachanged", "com.sonyericsson.music.playbackcontrol.ACTION_PLAYBACK_PAUSE", "com.sonyericsson.music.playbackcontrol.ACTION_PAUSED", "com.samsung.sec.android.MusicPlayer.playstatechanged", "com.samsung.sec.android.MusicPlayer.playbackcomplete", "com.samsung.sec.android.MusicPlayer.metachanged", "com.nullsoft.winamp.metachanged", "com.nullsoft.winamp.playstatechanged", "com.nullsoft.winamp.playbackcomplete", "com.amazon.mp3.metachanged", "com.amazon.mp3.playstatechanged", "com.amazon.mp3.playbackcomplete", "com.rdio.android.metachanged", "com.rdio.android.playbackcomplete", "com.rdio.android.playstatechanged", "com.spotify.music.metadatachanged", "com.spotify.music.playbackstatechanged", "com.spotify.music.queuechanged", "com.doubleTwist.androidPlayer.metachanged", "com.doubleTwist.androidPlayer.playstatechanged", "com.doubleTwist.androidPlayer.playbackcomplete", "org.iii.romulus.meridian.playbackcomplete", "org.iii.romulus.meridian.playstatechanged", "org.iii.romulus.meridian.metachanged", "com.tbig.playerpro.playstatechanged", "com.tbig.playerpro.metachanged", "com.tbig.playerpro.queuechanged", "com.tbig.playerpro.playbackcomplete"};
@@ -96,6 +118,11 @@ public class QuickEventsController {
         }
         mRunning = true;
         context.registerReceiver(mNowPlayingListener, intent);
+        IntentFilter psonalityIntent = new IntentFilter();
+        psonalityIntent.addAction(Intent.ACTION_TIME_TICK);
+        psonalityIntent.addAction(Intent.ACTION_TIME_CHANGED);
+        psonalityIntent.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        context.registerReceiver(mPSAListener, psonalityIntent);
         initQuickEvents();
     }
 
@@ -112,6 +139,7 @@ public class QuickEventsController {
         deviceIntroEvent();
         nowPlayingEvent();
         initNowPlayingEvent();
+        psonalityEvent();
     }
 
     private void deviceIntroEvent() {
@@ -216,6 +244,77 @@ public class QuickEventsController {
             status = false;
         }
         return status;
+    }
+
+    public void psonalityEvent() {
+        if (mEventIntro || mEventNowPlaying) return;
+
+        if (!Utilities.isQuickspacePersonalityEnabled(mContext)) return;
+
+        mEventTitle = Utilities.formatDateTime(mContext, System.currentTimeMillis());
+        mPSAMorningStr = mContext.getResources().getStringArray(R.array.quickspace_psa_morning);
+        mPSAEvenStr = mContext.getResources().getStringArray(R.array.quickspace_psa_evening);
+        mPSAMidniteStr = mContext.getResources().getStringArray(R.array.quickspace_psa_midnight);
+        mPSARandomStr = mContext.getResources().getStringArray(R.array.quickspace_psa_random);
+        int psaLength;
+
+        // Clean the onClick event to avoid any weird behavior
+        mEventTitleSubAction = new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // haha yes
+            }
+        };
+
+        switch (mPSACalendar.get(Calendar.HOUR_OF_DAY)) {
+            case 5: case 6: case 7: case 8: case 9:
+                psaLength = mPSAMorningStr.length - 1;
+                mEventTitleSub = mPSAMorningStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_morning;
+                mIsQuickEvent = true;
+                mEventPSA = true;
+                mImportantQuickEvent = false;
+                break;
+
+            case 19: case 20: case 21: case 22: case 23: case 0:
+                psaLength = mPSAEvenStr.length - 1;
+                mEventTitleSub = mPSAEvenStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_evening;
+                mIsQuickEvent = true;
+                mEventPSA = true;
+                mImportantQuickEvent = false;
+                break;
+
+            case 1: case 2: case 3: case 4:
+                if (getLuckyNumber(9) == 3) {
+                    psaLength = mPSAMidniteStr.length - 1;
+                    mEventTitleSub = mPSAMidniteStr[getLuckyNumber(0, psaLength)];
+                    mEventSubIcon = R.drawable.ic_quickspace_shishu;
+                } else {
+                    psaLength = mPSAEvenStr.length - 1;
+                    mEventTitleSub = mPSAEvenStr[getLuckyNumber(0, psaLength)];
+                    mEventSubIcon = R.drawable.ic_quickspace_evening;
+                }
+                mIsQuickEvent = true;
+                mEventPSA = true;
+                mImportantQuickEvent = false;
+                break;
+
+            default:
+                if (getLuckyNumber(13) == 7) {
+                    psaLength = mPSARandomStr.length - 1;
+                    mEventTitleSub = mPSARandomStr[getLuckyNumber(0, psaLength)];
+                    mEventSubIcon = R.drawable.ic_quickspace_btlg;
+                    mIsQuickEvent = true;
+                    mEventPSA = true;
+                    mImportantQuickEvent = false;
+                } else {
+                    mIsQuickEvent = false;
+                    mEventPSA = false;
+                }
+                break;
+        }
+
     }
 
     public boolean isQuickEvent() {

DerpFest-AOSP/android_packages_apps_DerpLauncher

Change-Id: I8d16e7612b8fc91a6919a2b44a581d289d46e138

src/com/android/launcher3/quickspace/QuickEventsController.java
@@ -32,6 +32,7 @@ import com.android.launcher3.LauncherFiles;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.R;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
 import java.util.ArrayList;
@@ -55,6 +56,28 @@ public class QuickEventsController {
     private boolean mIsFirstTimeDone = false;
     private SharedPreferences mPreferences;
 
+    // PSA + Personality
+    private boolean mEventPSA = false;
+    private String mPSAMessage;
+    private Calendar mPSACalendar = Calendar.getInstance();
+    private int mPSARandom;
+    private Random mPSAGenerator;
+    private String[] mPSAMorningStr;
+    private String[] mPSAEvenStr;
+    private String[] mPSAMidniteStr;
+    private String[] mPSARandomStr;
+    private BroadcastReceiver mPSAListener = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Calendar mPSAOldCalendar = mPSACalendar;
+            mPSACalendar = Calendar.getInstance();
+            if (mPSACalendar != mPSAOldCalendar) {
+                psonalityEvent();
+            }
+        }
+
+    };
+
     // NowPlaying
     private boolean mEventNowPlaying = false;
     private String mNowPlayingTitle;
@@ -71,6 +94,11 @@ public class QuickEventsController {
     public void initQuickEvents() {
         mPreferences = mContext.getSharedPreferences(LauncherFiles.SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE);
         mIsFirstTimeDone = mPreferences.getBoolean(SETTING_DEVICE_INTRO_COMPLETED, false);
+        IntentFilter psonalityIntent = new IntentFilter();
+        psonalityIntent.addAction(Intent.ACTION_TIME_TICK);
+        psonalityIntent.addAction(Intent.ACTION_TIME_CHANGED);
+        psonalityIntent.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        mContext.registerReceiver(mPSAListener, psonalityIntent);
         updateQuickEvents();
     }
 
@@ -78,6 +106,7 @@ public class QuickEventsController {
         deviceIntroEvent();
         nowPlayingEvent();
         initNowPlayingEvent();
+        psonalityEvent();
     }
 
     private void deviceIntroEvent() {
@@ -169,6 +198,67 @@ public class QuickEventsController {
                 }
             }
         };
+    }
+
+    public void psonalityEvent() {
+        if (mEventIntro || mEventNowPlaying) return;
+
+        if (!Utilities.isQuickspacePersonalityEnabled(mContext)) return;
+
+        mEventTitle = Utilities.formatDateTime(mContext, System.currentTimeMillis());
+        mPSAMorningStr = mContext.getResources().getStringArray(R.array.quickspace_psa_morning);
+        mPSAEvenStr = mContext.getResources().getStringArray(R.array.quickspace_psa_evening);
+        mPSAMidniteStr = mContext.getResources().getStringArray(R.array.quickspace_psa_midnight);
+        mPSARandomStr = mContext.getResources().getStringArray(R.array.quickspace_psa_random);
+        int psaLength;
+
+        // Clean the onClick event to avoid any weird behavior
+        mEventTitleSubAction = new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // haha yes
+            }
+        };
+
+        switch (mPSACalendar.get(Calendar.HOUR_OF_DAY)) {
+            case 5: case 6: case 7: case 8: case 9:
+                psaLength = mPSAMorningStr.length - 1;
+                mEventTitleSub = mPSAMorningStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_morning;
+                mIsQuickEvent = true;
+                mEventPSA = true;
+                break;
+
+            case 19: case 20: case 21: case 22: case 23: case 0:
+                psaLength = mPSAEvenStr.length - 1;
+                mEventTitleSub = mPSAEvenStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_evening;
+                mIsQuickEvent = true;
+                mEventPSA = true;
+                break;
+
+            case 1: case 2: case 3: case 4:
+                psaLength = mPSAEvenStr.length - 1;
+                mEventTitleSub = mPSAEvenStr[getLuckyNumber(0, psaLength)];
+                mEventSubIcon = R.drawable.ic_quickspace_evening;
+                mIsQuickEvent = true;
+                mEventPSA = true;
+                break;
+
+            default:
+                if (getLuckyNumber(13) == 7) {
+                    psaLength = mPSARandomStr.length - 1;
+                    mEventTitleSub = mPSARandomStr[getLuckyNumber(0, psaLength)];
+                    mEventSubIcon = R.drawable.ic_quickspace_derp;
+                    mIsQuickEvent = true;
+                    mEventPSA = true;
+                } else {
+                    mIsQuickEvent = false;
+                    mEventPSA = false;
+                }
+                break;
+        }
+
     }
 
     public boolean isQuickEvent() {