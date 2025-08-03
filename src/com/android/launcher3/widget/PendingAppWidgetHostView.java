f1e4f0810fdad98953d132eeb1ae1e936e686a51
NurKeinNeid <nurkeinneid@derpfest.org>
2025-08-03 19:47:48 +0200

    DerpLauncher: Update drawable retrieval in PageIndicatorDots and PendingAppWidgetHostView to use context.getResources().getDrawable()
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/com/android/launcher3/widget/PendingAppWidgetHostView.java
+++ src/com/android/launcher3/widget/PendingAppWidgetHostView.java
@@ -312,7 +312,7 @@ public class PendingAppWidgetHostView extends LauncherAppWidgetHostView
             mSettingIconDrawable = null;
         } else if (isReadyForClickSetup()) {
             mCenterDrawable = drawableFactory.newIcon(getContext(), info);
-            mSettingIconDrawable = getResources().getDrawable(R.drawable.ic_setting).mutate();
+            mSettingIconDrawable = getContext().getResources().getDrawable(R.drawable.ic_setting, getContext().getTheme()).mutate();
             updateSettingColor(info.bitmap.color);
 
             mDragFlags |= FLAG_DRAW_SETTINGS | FLAG_DRAW_LABEL;
