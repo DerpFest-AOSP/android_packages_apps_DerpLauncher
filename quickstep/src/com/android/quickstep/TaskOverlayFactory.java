killcmd/packages_apps_DerpLauncher

DerpLauncher: Show clear all button in recents overview
idoybh (YAAP): Keep rightmost

Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
Change-Id: I02ccb39cfbaa987ce2b6a0ae81702e9a34fb215e
Signed-off-by: Ratoriku <a1063021545@gmail.com>

quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -251,6 +251,11 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             overviewPanel.initiateSplitSelect(mThumbnailView.getTaskView());
         }
 
+        private void clearAllTasks() {
+            final RecentsView recentsView = mThumbnailView.getTaskView().getRecentsView();
+            recentsView.dismissAllTasks();
+        }
+
         /**
          * Called when the overlay is no longer used.
          */
@@ -358,12 +363,19 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             }
 
             @SuppressLint("NewApi")
+            @Override
             public void onScreenshot() {
                 endLiveTileMode(() -> saveScreenshot(mTask));
             }
 
+            @Override
             public void onSplit() {
                 endLiveTileMode(TaskOverlay.this::enterSplitSelect);
+            }
+
+            @Override
+            public void onClearAllTasksRequested() {
+                endLiveTileMode(TaskOverlay.this::clearAllTasks);
             }
         }
     }
@@ -378,5 +390,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
 
         /** User wants to start split screen with current app. */
         void onSplit();
+
+        void onClearAllTasksRequested();
     }
 }

DerpFest-AOSP/android_packages_apps_DerpLauncher

quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -256,6 +256,11 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
                     .saveAppPair(taskView);
         }
 
+        private void clearAllTasks() {
+             final RecentsView recentsView = mTaskContainer.getTaskView().getRecentsView();
+             recentsView.dismissAllTasks();
+        }
+
         /**
          * Called when the overlay is no longer used.
          */
@@ -402,16 +407,24 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             }
 
             @SuppressLint("NewApi")
+            @Override
             public void onScreenshot() {
                 endLiveTileMode(() -> saveScreenshot(mTask));
             }
 
+            @Override
             public void onSplit() {
                 endLiveTileMode(TaskOverlay.this::enterSplitSelect);
             }
 
+            @Override
             public void onSaveAppPair() {
                 endLiveTileMode(TaskOverlay.this::saveAppPair);
+            }
+
+            @Override
+            public void onClearAllTasksRequested() {
+                endLiveTileMode(TaskOverlay.this::clearAllTasks);
             }
         }
     }
@@ -429,5 +442,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
 
         /** User wants to save an app pair with current group of apps. */
         void onSaveAppPair();
+
+        void onClearAllTasksRequested();
     }
 }