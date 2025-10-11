9245a3f035f5a937175f7ce5c45153bdf7c051d7
Pranav Vashi <neobuddy89@gmail.com>
2025-10-12 02:07:13 +0530

    DerpLauncher: RecentsView: Clear DepthController base surface on every exit
    Change-Id: I01ed975c41d752f3c41ab03b9791ac1eefcad126
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -1239,6 +1239,9 @@ public abstract class RecentsView<
     protected void onWindowVisibilityChanged(int visibility) {
         super.onWindowVisibilityChanged(visibility);
         updateTaskStackListenerState();
+        if (visibility != VISIBLE && enableOverviewBackgroundWallpaperBlur()) {
+            mBlurUtils.setDrawLiveTileBelowRecents(false);
+        }
     }
 
     public void init(OverviewActionsView actionsView, SplitSelectStateController splitController,
