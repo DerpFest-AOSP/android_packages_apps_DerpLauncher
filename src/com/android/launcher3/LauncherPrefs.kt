862f1aff59b264a1545fff0ca8978dd849272d9f
NurKeinNeid <nurkeinneid@derpfest.org>
2025-12-27 22:32:16 +0100

    DerpLauncher: Add app drawer folder support
    Ref: LawnchairLauncher/lawnchair@448d718
    
    Co-authored-by: John Andrew Camu <werdna.jac@gmail.com>
    Change-Id: I7738df8516050fbff65a2ef8718be3644ec91daa
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    (cherry picked from commit 4f501038399a3d900cf14944bfc4691d6b87e080)

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -280,6 +280,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val DOCK_AI_MUSIC_SEARCH = backedUpItem("pref_dock_ai_music_search", false)
         @JvmField val QSB_OUTER_OPACITY = backedUpItem("pref_qsb_outer_opacity", 70)
         @JvmField val DRAWER_SEARCH = backedUpItem("pref_drawer_search", true)
+        @JvmField val DRAWER_LIST = backedUpItem("pref_drawer_list", true)
         @JvmField val FONT_SIZE = backedUpItem("pref_custom_font_size", 100)
         @JvmField val ICON_SIZE = backedUpItem("pref_custom_icon_size", 100)
         @JvmField val RECENTS_CHIPS = backedUpItem("pref_recents_chips", true)
