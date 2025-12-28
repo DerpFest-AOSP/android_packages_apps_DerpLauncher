e8418e7cb0e22440e39715cde58453df21fc4a44
Ido Ben-Hur <idoybh2@gmail.com>
2025-12-28 12:22:02 +0100

    DerpLauncher: Allow to always show page indicator dots
    Change-Id: Iaa635603378f1790cfe0e102a54fdd5f78961a4b
    (cherry picked from commit e1b7937acc861a68d362be9970c024958218957d)

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -277,6 +277,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val WORKSPACE_LOCK = backedUpItem("pref_workspace_lock", false)
         @JvmField val ENABLE_MINUS_ONE = backedUpItem("pref_enable_minus_one", true)
         @JvmField val ALLOW_WALLPAPER_ZOOMING = backedUpItem("pref_allow_wallpaper_zooming", true)
+        @JvmField val AUTO_HIDE_DOTS = backedUpItem("pref_auto_hide_dots", true)
         @JvmField val DOCK_AI_MUSIC_SEARCH = backedUpItem("pref_dock_ai_music_search", false)
         @JvmField val QSB_OUTER_OPACITY = backedUpItem("pref_qsb_outer_opacity", 70)
         @JvmField val DRAWER_SEARCH = backedUpItem("pref_drawer_search", true)
