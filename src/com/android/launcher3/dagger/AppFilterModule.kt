91432372c9bd12703eb68218962e635cffd4059f
NurKeinNeid <nurkeinneid@derpfest.org>
2026-01-08 00:05:40 +0100

    DerpLauncher: Fix hidden apps filtering and database SQL bugs
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

---
+++ src/com/android/launcher3/dagger/AppFilterModule.kt
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: DerpFest AOSP
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.launcher3.dagger
+
+import android.content.Context
+import com.android.launcher3.AppFilter
+import com.android.launcher3.lineage.trust.HiddenAppsFilter
+import dagger.Module
+import dagger.Provides
+
+@Module
+object AppFilterModule {
+
+    @Provides
+    fun provideAppFilter(@ApplicationContext context: Context): AppFilter {
+        return HiddenAppsFilter(context)
+    }
+}
