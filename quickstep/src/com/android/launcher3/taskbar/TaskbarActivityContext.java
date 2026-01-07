c1201f91d54077fb6e4123628b2a126426d4ba44
NurKeinNeid <nurkeinneid@derpfest.org>
2026-01-08 00:13:38 +0100

    DerpLauncher: Add app protection check when launching from recents and taskbar
    Show authentication screen before launching protected apps from recent
    tasks and taskbar to maintain security consistency across launcher
    interactions.
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
@@ -61,6 +61,7 @@ import android.animation.AnimatorSet;
 import android.animation.ValueAnimator;
 import android.app.ActivityOptions;
 import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo.Config;
@@ -105,6 +106,7 @@ import com.android.launcher3.LauncherSettings.Favorites;
 import com.android.launcher3.R;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.allapps.ActivityAllAppsContainerView;
+import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 import com.android.launcher3.anim.AnimatorListeners;
 import com.android.launcher3.anim.AnimatorPlaybackController;
 import com.android.launcher3.apppairs.AppPairIcon;
@@ -2014,6 +2016,26 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
             }
             // TODO(b/216683257): Use startActivityForResult for search results that require it.
             if (taskInRecents != null) {
+                // Check if app is protected before launching from recents
+                TrustDatabaseHelper db = TrustDatabaseHelper.getInstance(this);
+                ComponentName cn = info.getTargetComponent();
+                boolean isProtected = cn != null && db.isPackageProtected(cn.getPackageName());
+                
+                if (isProtected) {
+                    // Show authentication screen for protected apps
+                    Utilities.showLockScreen(this, getString(R.string.trust_apps_manager_name), () -> {
+                        // Re launch instance from recents after authentication
+                        ActivityOptionsWrapper authOpts = getActivityLaunchOptions(null, info);
+                        authOpts.options.setLaunchDisplayId(displayId);
+                        if (ActivityManagerWrapper.getInstance()
+                                .startActivityFromRecents(taskInRecents.key, authOpts.options)) {
+                            mControllers.uiController.getRecentsView()
+                                    .addSideTaskLaunchCallback(authOpts.onEndCallback);
+                        }
+                    });
+                    return;
+                }
+                
                 // Re launch instance from recents
                 if (ActivityManagerWrapper.getInstance()
                         .startActivityFromRecents(taskInRecents.key, opts.options)) {
--- quickstep/src/com/android/quickstep/TaskShortcutFactory.java
+++ quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -29,6 +29,8 @@ import android.app.ActivityManagerNative;
 import android.app.ActivityOptions;
 import android.app.IActivityManager;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Rect;
@@ -50,8 +52,11 @@ import androidx.annotation.Nullable;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.R;
 import com.android.launcher3.SecondaryDropTarget;
+import com.android.launcher3.Utilities;
+import com.android.launcher3.lineage.trust.db.TrustDatabaseHelper;
 import com.android.launcher3.logging.StatsLogManager.LauncherEvent;
 import com.android.launcher3.model.WellbeingModel;
+import com.android.launcher3.views.ActivityContext;
 import com.android.launcher3.popup.SystemShortcut;
 import com.android.launcher3.popup.SystemShortcut.AppInfo;
 import com.android.launcher3.model.data.ItemInfo;
@@ -226,8 +231,30 @@ public interface TaskShortcutFactory {
             final Task.TaskKey taskKey = mTaskContainer.getTask().key;
             final int taskId = taskKey.id;
             options.setSplashScreenStyle(SplashScreen.SPLASH_SCREEN_STYLE_ICON);
-            if (ActivityManagerWrapper.getInstance().startActivityFromRecents(taskId,
-                    options)) {
+            
+            // Check if app is protected before launching from recents
+            final Intent taskIntent = taskKey.baseIntent;
+            final String packageName = taskIntent != null && taskIntent.getComponent() != null 
+                    ? taskIntent.getComponent().getPackageName() : null;
+            final Context context = ((ActivityContext) mTarget).asContext();
+            final TrustDatabaseHelper db = TrustDatabaseHelper.getInstance(context);
+            final boolean isProtected = packageName != null && db.isPackageProtected(packageName);
+            
+            if (isProtected) {
+                // Show authentication screen for protected apps
+                Utilities.showLockScreen(context, context.getString(R.string.trust_apps_manager_name), () -> {
+                    if (ActivityManagerWrapper.getInstance().startActivityFromRecents(taskId, options)) {
+                        handleSuccessfulLaunch(taskId, taskKey);
+                    }
+                });
+            } else {
+                if (ActivityManagerWrapper.getInstance().startActivityFromRecents(taskId, options)) {
+                    handleSuccessfulLaunch(taskId, taskKey);
+                }
+            }
+        }
+        
+        private void handleSuccessfulLaunch(int taskId, Task.TaskKey taskKey) {
                 final Runnable animStartedListener = () -> {
                     // Hide the task view and wait for the window to be resized
                     // TODO: Consider animating in launcher and do an in-place start activity
@@ -270,7 +297,6 @@ public interface TaskShortcutFactory {
                         taskKey.displayId);
                 mTarget.getStatsLogManager().logger().withItemInfo(mTaskContainer.getItemInfo())
                             .log(mLauncherEvent);
-            }
         }
 
         /**
