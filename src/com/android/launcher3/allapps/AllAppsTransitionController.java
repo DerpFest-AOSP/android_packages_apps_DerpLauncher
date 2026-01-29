c28d0006b7aea7be057f4bde3a52b5d49551b46b
NurKeinNeid <nurkeinneid@derpfest.org>
2026-01-29 19:30:20 +0100

    DerpLauncher: Fix status bar scrim logic for all apps sheet
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/com/android/launcher3/allapps/AllAppsTransitionController.java
+++ src/com/android/launcher3/allapps/AllAppsTransitionController.java
@@ -249,8 +249,11 @@ public class AllAppsTransitionController
         boolean hasNavScrim = progress < NAV_BAR_COLOR_FORCE_UPDATE_THRESHOLD
                 && mLauncher.getAppsView().getNavBarScrimHeight() > 0;
         boolean hasStatusScrim = progress < STATUS_BAR_COLOR_FORCE_UPDATE_THRESHOLD;
+        // When using the bottom sheet, the status bar is over the dimmed/blurred scrim which
+        // appears dark regardless of theme, so use light status bar icons for visibility.
+        int statusScrimFlag = mShouldShowAllAppsOnSheet ? FLAG_DARK_STATUS : mStatusScrimFlag;
         mLauncher.getSystemUiController().updateUiState(UI_STATE_ALL_APPS,
-                ((hasNavScrim ? mNavScrimFlag : 0) | (hasStatusScrim ? mStatusScrimFlag : 0)));
+                ((hasNavScrim ? mNavScrimFlag : 0) | (hasStatusScrim ? statusScrimFlag : 0)));
     }
 
     public float getProgress() {
