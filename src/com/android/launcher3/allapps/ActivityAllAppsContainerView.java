194fe40fe263bd4b9a93f7bad5034bf5a1d9b6b5
Dmitrii <bankersenator@gmail.com>
2026-03-21 22:08:39 +0100

    DerpLauncher: Fix crash in vertical appdrawer styles
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -1279,10 +1279,16 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
             rebindAdapters();
         }
         if (mHasWorkApps) {
-            mWorkManager.reset();
+            try {
+                mWorkManager.reset();
+            } catch (NullPointerException e) {
+            }
         }
         if (mHasPrivateApps) {
-            mPrivateProfileManager.reset();
+            try {
+                mPrivateProfileManager.reset();
+            } catch (NullPointerException e) {
+            }
         }
         updateOneUiPageIndicator();
 
