LineageOS/android_packages_apps_Launcher3

Increase CPU clock speed when enabling early wakeup.
This was inadvertently missed in this previous refactor: ag/33432314

Flag: EXEMPT BUGFIX
Test: Builds; might run some perf tests
Change-Id: Id6406d93ba9008d8d4f4090454b0480ee747f1cf

quickstep/src/com/android/quickstep/util/BaseDepthController.java
@@ -270,6 +270,7 @@ public class BaseDepthController<
         if (start) {
             Trace.instantForTrack(TRACE_TAG_APP, TAG, "notifyRendererForGpuLoadUp");
             mContainer.getRootView().getViewRootImpl().notifyRendererForGpuLoadUp("applyBlur");
+            mContainer.getRootView().getViewRootImpl().notifyRendererOfExpensiveFrame("applyBlur");
             transaction.setEarlyWakeupStart(mEarlyWakeupInfo);
         } else {
             transaction.setEarlyWakeupEnd(mEarlyWakeupInfo);

DerpFest-AOSP/android_packages_apps_DerpLauncher

Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

quickstep/src/com/android/quickstep/util/BaseDepthController.java
@@ -297,6 +297,7 @@ public class BaseDepthController {
         if (start) {
             Trace.instantForTrack(TRACE_TAG_APP, TAG, "notifyRendererForGpuLoadUp");
             mLauncher.getRootView().getViewRootImpl().notifyRendererForGpuLoadUp("applyBlur");
+            mLauncher.getRootView().getViewRootImpl().notifyRendererOfExpensiveFrame("applyBlur");
             transaction.setEarlyWakeupStart(mEarlyWakeupInfo);
         } else {
             transaction.setEarlyWakeupEnd(mEarlyWakeupInfo);