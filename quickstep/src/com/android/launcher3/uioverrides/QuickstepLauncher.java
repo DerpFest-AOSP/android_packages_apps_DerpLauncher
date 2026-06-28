fe8dd250062a6a1fc54bf8ef5de2e086ae634140
Ghosuto <clash.raja10@gmail.com>
2026-06-28 19:58:42 +0200

    DerpLauncher: Add app rename feature
    * Allows users to rename apps from the home screen and app drawer.
    
    Co-authored-by: Pranav Praveen <72447760+TwistedVision518@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -62,6 +62,7 @@ import static com.android.launcher3.popup.SystemShortcut.INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.PAUSE_APPS;
 import static com.android.launcher3.popup.SystemShortcut.PRIVATE_PROFILE_INSTALL;
 import static com.android.launcher3.popup.SystemShortcut.REMOVE;
+import static com.android.launcher3.popup.SystemShortcut.RENAME_APP;
 import static com.android.launcher3.popup.SystemShortcut.UNINSTALL_APP;
 import static com.android.launcher3.popup.SystemShortcut.WIDGETS;
 import static com.android.launcher3.taskbar.LauncherTaskbarUIController.ALL_APPS_PAGE_PROGRESS_INDEX;
@@ -587,7 +588,8 @@ public class QuickstepLauncher extends Launcher implements RecentsViewContainer,
     public Stream<SystemShortcut.Factory> getSupportedShortcuts(ItemInfo itemInfo) {
         // Order matters as it affects order of appearance in popup container
         List<SystemShortcut.Factory> shortcuts = new ArrayList(Arrays.asList(
-                APP_INFO, WellbeingModel.SHORTCUT_FACTORY, mHotseatPredictionController));
+                APP_INFO, WellbeingModel.SHORTCUT_FACTORY, mHotseatPredictionController,
+                RENAME_APP));
         int container = itemInfo.container;
         if (canPinAppWithContextMenu()
                 && DisplayController.showDesktopTaskbarForFreeformDisplay(this)
--- src/com/android/launcher3/icons/IconCache.java
+++ src/com/android/launcher3/icons/IconCache.java
@@ -67,6 +67,7 @@ import com.android.launcher3.model.data.PackageItemInfo;
 import com.android.launcher3.model.data.WorkspaceItemInfo;
 import com.android.launcher3.pm.InstallSessionHelper;
 import com.android.launcher3.pm.UserCache;
+import com.android.launcher3.popup.SystemShortcut;
 import com.android.launcher3.shortcuts.ShortcutKey;
 import com.android.launcher3.shortcuts.ShortcutRequest;
 import com.android.launcher3.util.ApplicationInfoWrapper;
@@ -105,6 +106,7 @@ public class IconCache extends BaseIconCache {
     private static final String TAG = "Launcher.IconCache";
 
     private final LauncherApps mLauncherApps;
+    private final Context mContext;
     private final UserCache mUserManager;
     private final InstallSessionHelper mInstallSessionHelper;
     private final InstantAppResolver mInstantAppResolver;
@@ -128,6 +130,7 @@ public class IconCache extends BaseIconCache {
             DaggerSingletonTracker lifecycle) {
         super(context, dbFileName, MODEL_EXECUTOR.getLooper(),
                 idp.fillResIconDpi, idp.iconBitmapSize, true /* inMemoryCache */, iconProvider);
+        mContext = context;
         mLauncherApps = context.getSystemService(LauncherApps.class);
         mUserManager = userCache;
         mInstallSessionHelper = installSessionHelper;
@@ -630,6 +633,10 @@ public class IconCache extends BaseIconCache {
     protected void applyCacheEntry(@NonNull final CacheEntry entry,
             @NonNull final ItemInfoWithIcon info) {
         info.title = Utilities.trim(entry.title);
+        String customTitle = SystemShortcut.getCustomName(mContext, info);
+        if (customTitle != null) {
+            info.title = customTitle;
+        }
         info.contentDescription = entry.contentDescription;
         info.bitmap = entry.bitmap;
         // Clear any previously set appTitle, if the packageOverride is no longer valid
@@ -654,6 +661,10 @@ public class IconCache extends BaseIconCache {
         }
         info.appTitle = Utilities.trim(info.title);
         info.title = Utilities.trim(packageEntry.title);
+        customTitle = SystemShortcut.getCustomName(mContext, info);
+        if (customTitle != null) {
+            info.title = customTitle;
+        }
         info.contentDescription = packageEntry.contentDescription;
         info.bitmap = packageEntry.bitmap;
     }
