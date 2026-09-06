DerpFest-12/packages_apps_DerpLauncher

DerpLauncher: add toggle for desktop and drawer labels
Squashed with:

Author: razorloves <razorloves@gmail.com>
Date:   Wed Jan 8 23:53:25 2020 -0600

    Trebuchet: Fix missing icon labels in folders

    Fixes regression introduced in 186796e.

    Change-Id: I1e6054fe84ea54bf2a85bf6d26760196ffe78f69

Author: LuK1337 <priv.luk@gmail.com>
Date:   2021-01-13 17:01:22 +0100

    Trebuchet: Honor desktop show labels preference for folder icons

    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/2937

    Change-Id: I5769057809890cd8086ec01d6e9d1be87fe17c25

Change-Id: I98063b7adaf22029c4bfa50d2cac730f3612e121
Signed-off-by: Joey <joey@lineageos.org>

src/com/android/launcher3/folder/FolderIcon.java
@@ -194,7 +194,9 @@ public class FolderIcon extends FrameLayout implements FolderListener, IconLabel
 
         icon.setClipToPadding(false);
         icon.mFolderName = icon.findViewById(R.id.folder_icon_name);
-        icon.mFolderName.setText(folderInfo.title);
+        if (icon.mFolderName.shouldShowLabel()) {
+            icon.mFolderName.setText(folderInfo.title);
+        }
         icon.mFolderName.setCompoundDrawablePadding(0);
         FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) icon.mFolderName.getLayoutParams();
         lp.topMargin = grid.iconSizePx + grid.iconDrawablePaddingPx;

DerpFest-AOSP/android_packages_apps_DerpLauncher

    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date: Mon, 31 Mar 2025 09:40:19 +0530
    fixup! DerpLauncher: Add toggle for desktop and drawer labels
    Change-Id: I369c10c6afc9f9f1ad8621ea28b63232af5c6f6a
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
Co-authored-by: razorloves <razorloves@gmail.com>
Co-authored-by: LuK1337 <priv.luk@gmail.com>
Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

src/com/android/launcher3/folder/FolderIcon.java
@@ -226,7 +226,9 @@ public class FolderIcon extends FrameLayout implements FloatingIconViewCompanion
 
         icon.setClipToPadding(false);
         icon.mFolderName = icon.findViewById(R.id.folder_icon_name);
-        icon.mFolderName.applyLabel(folderInfo.title);
+        if (icon.mFolderName.shouldShowLabel()) {
+            icon.mFolderName.applyLabel(folderInfo.title);
+        }
         icon.mFolderName.setCompoundDrawablePadding(0);
         FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) icon.mFolderName.getLayoutParams();
         lp.topMargin = grid.getWorkspaceProfile().getIconSizePx()
@@ -720,7 +722,9 @@ public class FolderIcon extends FrameLayout implements FloatingIconViewCompanion
     }
 
     public void onTitleChanged(CharSequence title) {
-        mFolderName.applyLabel(title);
+        if (mFolderName.shouldShowLabel()) {
+            mFolderName.applyLabel(title);
+        }
         setContentDescription(getAccessiblityTitle(title));
     }
 