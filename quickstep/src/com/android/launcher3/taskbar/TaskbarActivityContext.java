633888275dcf1e79596d48317f3def205ac0e81b
LuK1337 <priv.luk@gmail.com>
2026-09-16 10:07:23 +0200

    DerpLauncher: Keep the navigation bar at the bottom on large screens
    Phone mode assumes that the navigation bar moves to the side of the
    display in landscape, which is only true for phones. Large screens
    entering phone mode because the taskbar is disabled ended up with a
    window on the side of the display and buttons laid out vertically, which
    left the navigation bar empty.
    
    Assisted-by: LLM
    Change-Id: I0ce3ee7e75eba5abb53bcff1e37a6da2d8269d7f

--- quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
@@ -903,7 +903,8 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
             WindowManager.LayoutParams lp =
                     createDefaultWindowLayoutParams(windowType,
                             TaskbarActivityContext.WINDOW_TITLE);
-            if (isPhoneButtonNavMode()) {
+            if (isPhoneButtonNavMode()
+                    && mDeviceProfile.getDeviceProperties().getCanNavBarMove()) {
                 populatePhoneButtonNavModeWindowLayoutParams(rot, lp);
             }
             windowLayoutParams.paramsForRotation[rot] = lp;
@@ -1580,7 +1581,9 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
     public void setTaskbarWindowSize(int size) {
         // In landscape phone button nav mode, we should set the task bar width instead of height
         // because this is the only case in which the nav bar is not on the display bottom.
-        boolean landscapePhoneButtonNav = isPhoneButtonNavMode() && mDeviceProfile.getDeviceProperties().isLandscape();
+        boolean landscapePhoneButtonNav = isPhoneButtonNavMode()
+                && mDeviceProfile.getDeviceProperties().getCanNavBarMove()
+                && mDeviceProfile.getDeviceProperties().isLandscape();
         if ((landscapePhoneButtonNav ? mWindowLayoutParams.width : mWindowLayoutParams.height)
                 == size || mIsDestroyed) {
             return;
